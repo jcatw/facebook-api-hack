8605263e3ed48259223cd8ffb618a339a7dc3ed3
James Atwood <jatwood@cs.umass.edu>
2014-06-13 16:16:27 -0700

    hmm

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
+// model
+var highlight_regexp = /([A-Z]\w*)/g;
+
+function input_hack() {
+		var s = myTextarea.value;
+		s = s.replace( highlight_regexp, '<span class="highlight">$1</span>');
+		myOtherTextarea.innerHTML = s.replace(/\r\n|\r|\n/gi, '<br/>');
+}
+
+// page setup
 $(document).ready(function() {
 		console.log("doc ready");
+		// load facebook sdk
 		$.ajaxSetup({ cache: true });
 		$.getScript('//connect.facebook.net/en_UK/all.js', function(){
 				FB.init({
 						appId: '450584821744469',
 				});
 		});
+		// set up post submission event
 		$("#submitter").on("click", function() {
 				console.log("submit clicked");
+				console.log($("#postarea").val());
 				FB.login(function(){
 						FB.api('/me/feed',
 									 'post',
@@ -18,4 +31,8 @@ $(document).ready(function() {
 				}, {scope:'publish_actions'});
 		});
 });
+
+
+
+
 
