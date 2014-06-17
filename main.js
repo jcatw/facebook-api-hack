e951e114953534bacd4cd2b1900ff5ce72042538
James Atwood <jatwood@cs.umass.edu>
2014-06-17 16:11:55 -0700

    much better interface, richer features

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
-// model
-var highlight_regexp = /([A-Z]\w*)/g;
+// features
+function featureFactory(regexp, description) {
+		return {'regexp': regexp, 'description': description};
+}
+		
+var features = [featureFactory(/([A-Z]\w*)/g, "Capitalized"),
+								featureFactory(/(spaz)/g, "spaz"),
+								featureFactory(/(Apple|Orange)/g, "Specific Fruit")];
+								
 
 // page setup
 $(document).ready(function() {
 		console.log("doc ready");
+		
 		// load facebook sdk
 		$.ajaxSetup({ cache: true });
 		$.getScript('//connect.facebook.net/en_UK/all.js', function(){
@@ -24,22 +32,41 @@ $(document).ready(function() {
 				}, {scope:'publish_actions'});
 		});
 		// text entry event
-		$("#postArea").on("keyup", function() {
+		//var word = "";
+		$("#postArea").on("keyup", function(event) {
+				//var wordbound = /\W/;
+				//var character = String.fromCharCode(event.which);
+				//console.log(character);
+				//$("#postMirror").html($("#postMirror").html().concat(character));
+				//word += character;
 				var s = $( this ).val();
+				//if(wordbound.test(character)) {
 				var count = 0;
-
+						
 				// count
 				var splt = s.split(/\W/);
 				for (var i = 0; i < splt.length; i++) {
-						if (splt[i].match(highlight_regexp)) {
-								count = count + 1;
+						for(var j = 0; j < features.length; j++) {
+								if (splt[i].match(features[j].regexp)) {
+										count = count + 1;
+								}
 						}
 				}
 				$("#count").text(count);
-
+				
 				// highlight
-				s = s.replace( highlight_regexp, '<span class="highlight">$1</span>');
-				otherPostArea.innerHTML = s.replace(/\r\n|\r|\n/gi, '<br/>');
+				
+				for(var j = 0; j < features.length; j++) {
+						var highlighter = '<span class="text-warning toggle" data-toggle="tooltip" data-placement="top" title="' + features[j].description + '">$1</span>';
+						s = s.replace( features[j].regexp , highlighter );
+						$("#postMirror").html(s.replace(/\r\n|\r|\n/gi, '<br/>'));
+				}
+				$(".toggle").tooltip();
+				//word = word.replace( highlight_regexp, '<span class="text-primary">$1</span>');
+				//$("#postMirror").html($("#postMirror").html().concat(word));
+				//word = "";
+				//else
+
 		});
 });
 
